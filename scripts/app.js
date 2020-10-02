f4fdf83e30d6caab8f38f9ce1d1f828fc7d26185
Jiang Hao <giovannilance94@gmail.com>
2020-10-03 00:32:16 +0800

    Updates for animation and microinteractions

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
 // [].forEach.call(document.querySelectorAll("*"), function (a) { a.style.outline = "1px solid #" + (~~(Math.random() * (1 << 24))).toString(16) })
+let indicator = document.querySelector('.indicator');
+
+document.querySelectorAll('.nav-link.link').forEach((li, ind) =>
+ li.addEventListener('mouseover', () => indicator.className = `hover-${ind + 1}`));
+document.querySelectorAll('.side-nav-link a').forEach((a, ind) =>
+ a.addEventListener('click', () => indicator.className = `hover-${ind + 1}`))
+
+document.querySelectorAll('.screen').forEach(
+ (screen, index) => {
+  window.addEventListener(
+   'scroll', function () {
+    if (index === 0) { return }
+    if (window.scrollY >= 640 * (index + 1) - window.innerHeight) {
+     screen.classList.add('animated');
+     console.log(screen.className);
+     window.removeEventListener('scroll', arguments.callee)
+    }
+   }
+  )
+ }
+)
+
+document.querySelector('.continue button').addEventListener('click', () => {
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+})
